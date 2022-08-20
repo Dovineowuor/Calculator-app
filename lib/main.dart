58b01717c6c1ecad7b9b23ecb6bb245c9136103c
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-08-20 10:16:29 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:calculator_app/pages/main_application.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  );
+
+  runApp(Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainApplication(),
+    );
+  }
+}
