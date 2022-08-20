413fdfdb0ff21b125ca86bd21e3e19a72889282a
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-08-20 10:16:17 +0300

    Create constant.dart

---
+++ lib/data/constant.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//colors
+
+Color white = Colors.white;
+Color black = Colors.black;
+Color blueGrey700 = Color(0xff455A64);
+Color blueGrey900 = Color(0xff263238);
+Color green = Color(0xff4CAF50);
+Color orange = Color(0xffFF5722);
