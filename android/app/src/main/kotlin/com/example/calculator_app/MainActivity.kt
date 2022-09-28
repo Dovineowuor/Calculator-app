e35c105cef13aadccf3c8d74109a704e98138b49
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-09-28 17:28:13 +0300

    UPDATE

--- android/app/src/main/kotlin/com/example/calculator_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/calculator_app/MainActivity.kt
@@ -5,3 +5,26 @@ import io.flutter.embedding.android.FlutterActivity
 class MainActivity: FlutterActivity() {
 //main activity
 }
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+// ...
+
+onCreate()
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private AdView mAdView;
+    // ...
+
+    @Override
+    protected void androidx.compose.foundation.layout.Box {
+        onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            mAdView = findViewById(R.id.adView);
+            AdRequest adRequest = new AdRequest.Builder().build();
+            mAdView.loadAd(adRequest);
+        }
+
